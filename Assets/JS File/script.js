2d542800dcd01bf7f1aec7b0d9757c2506c6bee6
Brayden Burleigh <brayburl224@gmail.com>
2022-09-25 19:43:06 -0400

    added necessary variables that will be needed for fetching API data later

--- Assets/JS File/script.js
+++ Assets/JS File/script.js
@@ -2,6 +2,14 @@ var APIKey = "f3659936b4dd484b16332e8037972e1e";
 
 var city;
 
+var temp;
+
+var wind;
+
+var humidity;
+
+var UVIndex;
+
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?city=" + city + "&appid=" + APIKey;
 
 fetch(queryURL);
