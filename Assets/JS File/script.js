57a6d6fa615e9ea7828390d385e77fd0bcc138a7
Brayden Burleigh <brayburl224@gmail.com>
2022-10-05 18:29:00 -0400

    worked more on the JS - fetching data from API

--- Assets/JS File/script.js
+++ Assets/JS File/script.js
@@ -10,6 +10,22 @@ var humidity;
 
 var UVIndex;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?city=" + city + "&appid=" + APIKey;
+function getApi() {
 
-fetch(queryURL);
+city = "Boston";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+    console.log(data.main.temp)
+  })
+
+  
+}
+
+getApi()
