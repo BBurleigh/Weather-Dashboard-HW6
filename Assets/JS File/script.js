ec29a7e1c569dc23f0a6ca83969fae315273fece
Brayden <brayburl224@gmail.com>
2022-11-13 16:32:03 -0500

    erased old HTML and started over with a new file + added other stylesheets + jquery and bootstrap js

--- Assets/JS File/script.js
+++ Assets/JS File/script.js
@@ -1,20 +1,8 @@
 var APIKey = "f3659936b4dd484b16332e8037972e1e";
 
-var city;
-
-var temp;
-
-var wind;
-
-var humidity;
-
-var UVIndex;
-
 function getApi() {
 
-city = "Boston";
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 fetch(queryURL)
   .then(function (response) {
@@ -29,3 +17,4 @@ fetch(queryURL)
 }
 
 getApi()
+
