bf420b33aa67830e62a514a71ae3c94f68c8afbe
Brayden <brayburl224@gmail.com>
2022-11-13 19:43:47 -0500

    draft of new city search button listener

--- Assets/JS File/script.js
+++ Assets/JS File/script.js
@@ -1,10 +1,16 @@
 var APIKey = "f3659936b4dd484b16332e8037972e1e";
 
+var currentCity = "";
+
+var lastCity = ""
+
 function getApi() {
 
 var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 let city = `${city}`;
+
+currentCity = city;
 
 fetch(queryURL)
   .then(((function (response) {
@@ -27,7 +33,17 @@ fetch(queryURL)
   
 }
 
+$('#search-button').on('click', (event) => {
 
+  event.preventDefault();
+
+  $('#search-city').val(event.target.textContent);
+
+  currentCity = $('#search-city').val();
+
+  getApi(event);
+
+})
 
 getApi()
 
